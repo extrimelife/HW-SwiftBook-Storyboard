8e62c6a46e71e16deecf5759f1e486b59e6d2e33
Roman Khilchenko <extrimelife@hotmail.com>
2022-07-25 22:36:47 +0300

    Initial Commit

---
+++ HW/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HW
+//
+//  Created by roman Khilchenko on 25.07.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
